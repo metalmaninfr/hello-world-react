73735ea939dca325c6348237ef217961f769497d
metalmaninfr <alex_blandford@hotmail.com>
2018-07-16 12:35:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+class App extends Component {
+  render() {
+    return (
+      <HelloWorld />
+    );
+  }
+}
+
+export default App;
